ff84168a275643b9dc6952ccaf29d3371d2310fa
@webEducation <alibabighef718@gmail.com>
2022-10-07 11:22:59 +0100

    upload snack game project created by html css and js you need for execute that game and play it a live server and open file html with live server

---
+++ game2.js
@@ -0,0 +1,15 @@
+export const snak_speed = 1;
+
+
+export function update() {
+
+    console.log("update")
+
+}
+
+export function draw() {
+
+    console.log("drawing")
+
+
+}
---
+++ js/jsFile.js
@@ -0,0 +1,44 @@
+
+export function upSound() {
+
+    let myAudio = new Audio();
+    myAudio.src = "audio/up.mpeg";
+    myAudio.play();
+
+}
+export function downSound() {
+
+    let myAudio = new Audio();
+    myAudio.src = "audio/down.mpeg";
+    myAudio.play();
+
+}
+export function leftSound() {
+
+    let myAudio = new Audio();
+    myAudio.src = "audio/left.mpeg";
+    myAudio.play();
+
+}
+export function rightSound() {
+
+    let myAudio = new Audio();
+    myAudio.src = "audio/right.mpeg";
+    myAudio.play();
+
+}
+
+export function eatSound() {
+
+    let myAudio = new Audio();
+    myAudio.src = "audio/eat.mpeg";
+    myAudio.play();
+
+}
+export function deadSound() {
+
+    let myAudio = new Audio();
+    myAudio.src = "audio/dead.mpeg";
+    myAudio.play();
+
+}
